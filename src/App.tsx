1ce670eb12e8313ca2a61971f48609525e0f6cd7
algazza <fadhilaswadira72564@gmail.com>
2024-10-29 23:09:48 +0700

    Latar Belakang

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import Header from "./components/Header";
-import MuseumPage from "./Section/MuseumPage.tsx";
 import LandingPage from "./Section/LandingPage.tsx";
 import ReactLenis from "lenis/react";
 import { motion, useInView } from "framer-motion";
 import RoundedPage from "./Section/RoundedPage.tsx";
-import { styles } from "./helper/index.ts";
-import SectionVidBg from "./Section/SectionVidBg.tsx";
+import { image1, image2, styles } from "./helper/index.ts";
+import Horizontal from "./Horizontal.tsx";
+import SecttionRoundLeft from "./Section/SecttionRoundLeft.tsx";
+import SectionRound from "./Section/SectionRound.tsx";
 
 const App = () => {
   const [bgColor, setBgColor] = useState("bg-backgroundColor");
@@ -33,22 +34,63 @@ const App = () => {
     };
   }, [isInView]);
 
-
   return (
-    <div className={`${bgColor} ${styles.fontBody} font-Playfair transition-colors duration-500`}>
+    <div
+      className={`${bgColor} ${styles.fontBody} font-Playfair transition-colors duration-500`}
+    >
       <ReactLenis root options={{ lerp: 0.095 }}>
         <Header />
         <main className="py-[6rem]">
-          <motion.section ref={ref} className="">
+          <section  className="">
             <LandingPage />
-          </motion.section>
-
-          <section className={`${styles.paddingY} px-6 md:px-0`}>
-            <SectionVidBg/>
           </section>
+
+          <motion.section ref={ref} className={`${styles.paddingY} px-6 md:px-0`}>
+          <SectionRound
+              rowReverse="flex-row-reverse"
+              imageDocs={image2}
+              roundedImg="rounded-bl-[5rem]"
+              roundedText="rounded-tl-[5rem] pl-8 left-[8rem]"
+              shadowPlace="-top-12 -left-12"
+              title="Kehadiran Pasukan Sekutu"
+              description={`Sekutu mulai memasuki Bandung pada akhir tahun 1945. Mereka 
+                ingin menguasai kota Bandung dan melucuti senjata pejuang Indonesia, yang 
+                menimbulkan ketegangan antara rakyat Indonesia dan pasukan Sekutu.`}
+            />
+
+            <div className="rounded-full border-2 border-black p-4 pl-10 bg-backgroundColor w-2/3 mx-auto">
+              <h1 className={styles.heading4}>Dilema Pejuang</h1>
+              <p className="mt-2">
+                Pada awal Maret 1946, Sekutu mengeluarkan ultimatum kepada para
+                pejuang dan warga Bandung agar mengosongkan kota bagian selatan
+                dalam waktu 24 jam. Jika tidak, mereka mengancam akan
+                menggunakan kekuatan militer.
+              </p>
+            </div>
+
+            <SectionRound
+              rowReverse=""
+              imageDocs={image1}
+              roundedImg="rounded-tr-[5rem]"
+              roundedText="rounded-br-[5rem] right-[8rem]"
+              shadowPlace="bottom-0 -right-12"
+              title="Situasi Pasca-Proklamasi"
+              description={`Setelah Proklamasi Kemerdekaan pada 17 Agustus 1945, 
+                kondisi di Indonesia masih belum stabil. Di banyak wilayah, pasukan 
+                Sekutu (yang dipimpin Inggris) kembali masuk ke Indonesia dengan tujuan 
+                melucuti tentara Jepang yang sudah kalah pada Perang Dunia II. Namun, 
+                seringkali pasukan Sekutu juga membantu Belanda untuk kembali menguasai 
+                wilayah Indonesia.`}
+            />
+
+          </motion.section>
 
           <section className={`${styles.marginX} ${styles.paddingY}`}>
             <RoundedPage />
+          </section>
+
+          <section>
+            <Horizontal />
           </section>
         </main>
       </ReactLenis>
---
+++ src/Section/SectionRound.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { illustrasi, image1, styles, video } from "../helper";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+
+type SectionRoundRightProps = {
+  rowReverse: string
+  imageDocs: string
+  roundedImg: string
+  roundedText: string
+  shadowPlace: string
+  title: string
+  description: string
+}
+
+const SectionRound = ({rowReverse, imageDocs, roundedImg, roundedText, shadowPlace, title, description}: SectionRoundRightProps) => {
+  return (
+    <div
+      className={`md:flex relative py-12 ${rowReverse}`}
+    >
+      <div className="">
+        <img
+          src={imageDocs}
+          alt="Dokumentasi Bandung Lautan Api"
+          className={`${roundedImg} w-full md:h-[25rem]`}
+        />
+      </div>
+
+      <div className={`absolute ${shadowPlace} w-[300px] h-[300px] bg-accent blur-[120px]`} />
+
+      <div className={`${roundedText} border-2 border-black p-3 md:absolute md:w-1/3 md:top-1/3 right-[8rem] bg-backgroundColor`}>
+        <h1 className={styles.heading4}>{title}</h1>
+        <p className="mt-2">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SectionRound;
